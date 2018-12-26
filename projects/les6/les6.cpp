dda4ab76265cc43527e2d3fa7d5a61d6c86de0f5
Crazy-Dog <ilya-drachev@mail.ru>
2018-12-26 21:13:55 +0300

    fix 6th lesson

--- projects/les6/les6.cpp
+++ projects/les6/les6.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+class A
+{
+	int b;
+public:
+	A(int n) 
+	{
+		b = n;
+	}
+	~A(){}
+	int get() {return b;}
+	bool operator<(A obj)
+	{
+		return (b < obj.b);
+	}
+};
+
+int main()
+{
+	A ob1(1);
+	A ob2(9);
+	std::cout << (ob1 < ob2) << std::endl;
+}
